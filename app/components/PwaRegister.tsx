5b14520a9ea883fa62c15f2fca241d83bb088ecf
HEET JANI <heet2404@gmail.com>
2026-08-09 15:27:15 +0530

    Avoid PWA cache during local development

--- app/components/PwaRegister.tsx
+++ app/components/PwaRegister.tsx
@@ -4,9 +4,15 @@ import { useEffect } from "react";
 
 export function PwaRegister() {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
-      void navigator.serviceWorker.register("/sw.js");
+    if (!("serviceWorker" in navigator)) return;
+
+    if (!import.meta.env.PROD) {
+      void navigator.serviceWorker.getRegistrations().then((registrations) => Promise.all(registrations.map((registration) => registration.unregister())));
+      void caches.keys().then((keys) => Promise.all(keys.map((key) => caches.delete(key))));
+      return;
     }
+
+    void navigator.serviceWorker.register("/sw.js");
   }, []);
 
   return null;
